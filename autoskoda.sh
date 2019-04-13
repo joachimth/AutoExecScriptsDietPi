e836189db60beed8aae3cba8547c95f5e91d6dab
joachimth <46874512+joachimth@users.noreply.github.com>
2019-04-14 00:02:20 +0200

    Update autoskoda.sh

--- autoskoda.sh
+++ autoskoda.sh
@@ -1,5 +1,7 @@
-sudo apt-get install git
-git clone https://github.com/joachimth/SuperbRapi-FV
-cd ~/SuperbRapi
+#!/bin/bash
+sudo apt-get install git -y
+cd ~
+git clone https://github.com/joachimth/PureSuperbRapi 
+cd ~/PureSuperbRapi
 chmod +x setup.sh
 ./setup.sh
