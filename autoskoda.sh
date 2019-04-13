61433b98f96e288ec841a5d05e9cc1ae91483ea4
joachimth <46874512+joachimth@users.noreply.github.com>
2019-04-14 00:47:54 +0200

    Update autoskoda.sh

--- autoskoda.sh
+++ autoskoda.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-Echo "Hi there you are entering the wild world of automation"
+echo -e "Hi there you are entering the wild world of automation"
 sleep 5
 sudo apt-get install git -y
 cd ~
@@ -9,3 +9,4 @@ git clone https://github.com/joachimth/PureSuperbRapi
 cd ~/PureSuperbRapi
 chmod +x setup.sh
 ./setup.sh
+echo -e "Hi there you are LEAVING!! the wild world of automation"
