879cd53a3ac4a4e33a23489ae72d0fb2f70bfd60
joachimth <46874512+joachimth@users.noreply.github.com>
2019-04-14 23:46:37 +0200

    Create SuperbPi.sh

---
+++ SuperbPi.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+echo -e "Hi there, you are entering the wild world of automation"
+sudo apt-get install git -y
+sleep 5
+cd ~
+rm -rf ~/SuperbPi
+git clone https://github.com/joachimth/SuperbPi
+cd ~/SuperbPi
+chmod +x cleanstart.sh
+./cleanstart.sh
+rm -rf ~/SuperbPi/cleanstart.sh
+
+echo -e "Hi there you are LEAVING!! the wild world of automation"
