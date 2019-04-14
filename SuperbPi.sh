2505dcc3d0d11ff28b48355d49b39807facfe28a
joachimth <46874512+joachimth@users.noreply.github.com>
2019-04-15 00:14:13 +0200

    Update SuperbPi.sh

--- SuperbPi.sh
+++ SuperbPi.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 
 echo "Hi there, you are entering the wild world of automation"
-sudo apt-get install git -y &>/dev/null
+
+sudo apt-get install git -y
 sleep 5
-cd ~ &>/dev/null
-rm -rf ~/SuperbPi &>/dev/null
+cd ~
+rm -rf ~/SuperbPi
 git clone https://github.com/joachimth/SuperbPi
-cd ~/SuperbPi &>/dev/null
-chmod +x cleanstart.sh &>/dev/null
+cd ~/SuperbPi
+chmod +x cleanstart.sh
 ./cleanstart.sh
-rm -rf ~/SuperbPi/cleanstart.sh &>/dev/null
+rm -rf ~/SuperbPi/cleanstart.sh
 
 echo "Hi there you are LEAVING!! the wild world of automation"
